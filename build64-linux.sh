a5106bda04a02d9196da82b1094a7373f2e8bc80
Jon Daniel <jopadan@mailfence.com>
2024-05-02 11:50:18 +0200

    restructe source and make tlsf a submodule

---
+++ build64-linux.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+clear
+
+#if false; then
+
+rm -f *.o
+rm -f *.map
+rm -f bin/tube64
+
+#echo 'Press any key...'
+#read -rsn1
+
+INCLUDES="-Isrc/game -Isrc/music -Isrc/sound -Ideps/tlsf -Isrc/tlsf"
+CXXFLAGS+="-m64 -Ofast -DNDEBUG -ffunction-sections -fdata-sections -fno-rtti -ftree-vectorize -fno-math-errno -fmax-errors=1 -fomit-frame-pointer -ffast-math -fno-exceptions -fno-unwind-tables -fno-asynchronous-unwind-tables -Wall -Wno-unused-variable -Wno-unused-but-set-variable -Wno-paranthese -Wno-maybe-uninitialized -Wno-unused-result -Wno-format-security ${INCLUDES}"
+
+
+echo 'Compile tube...'
+g++ -m64 ${CXXFLAGS} -c src/game/tube.cpp -fsigned-char
+g++ -m64 ${CXXFLAGS} -c src/game/trig.cpp
+g++ -m64 ${CXXFLAGS} -c src/game/misc.cpp
+g++ -m64 ${CXXFLAGS} -D_OPL3_ -c src/music/hmp.cpp
+g++ -m64 ${CXXFLAGS} -D_OPL3_ -c src/sound/sb16.cpp
+gcc -m64 ${CXXFLAGS} -c src/music/hmpfile.c
+gcc -m64 ${CXXFLAGS} -c src/music/hmpopl.c
+gcc -m64 ${CXXFLAGS} -c src/music/opl3.c -o opl.o
+gcc -m64 ${CXXFLAGS} -c deps/tlsf/tlsf.c
+g++ -m64 ${CXXFLAGS} -c src/tlsf/allocator.cpp
+
+#fi
+
+echo 'Link tube...'
+g++ -m64 -Ofast -DNDEBUG -o bin/tube64 tube.o trig.o misc.o opl.o hmpopl.o hmpfile.o hmp.o sb16.o tlsf.o allocator.o -static-libstdc++ -static-libgcc -lSDL2 -lasound -Xlinker -Map=tube.map -Wl,--strip-all -Wl,--gc-sections
+
+chmod +x bin/tube64
+
